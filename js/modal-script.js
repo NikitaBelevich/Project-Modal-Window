c3fe646b188d622ef9784e2645b02d4e36a72909
Nikita Belevich <belevish_1998@mail.ru>
2020-08-20 22:56:07 +0300

    feat: Добавлена возможность открытия и скрытия окна

--- js/modal-script.js
+++ js/modal-script.js
@@ -1,2 +1,37 @@
 'use strict';
 
+// Открытие окна
+document.addEventListener('click', event => {
+    const targetButtonShow = event.target.closest('.modal-show');
+    if (!targetButtonShow) return;
+    // Уверены что это была нажата кнопка вызова окна, запускаем функцию показа
+    showModal.call(targetButtonShow); // Контекстом будет целевая кнопка
+});
+
+function showModal() {
+    const typeModal = this.dataset.modal;
+    const modalWrap = document.querySelector(typeModal).parentElement;
+    // Показываем наш оверлей и окно
+    modalWrap.classList.remove('hide-modal');
+}
+
+// Закрытие модального окна
+const modalWindow = document.querySelector('.modal-window');
+modalWindow.addEventListener('click', event => {
+    const targetCloseButton = event.target.closest('.close-modal');
+    if (!targetCloseButton) return;
+
+    closeModal.call(targetCloseButton);
+});
+
+function closeModal() {
+    const typeModal = this.dataset.modal;
+    const modalWrap = document.querySelector(typeModal).parentElement;
+    // Показываем наш оверлей и окно
+    modalWrap.classList.add('hide-modal');
+}
+
+
+
+
+
