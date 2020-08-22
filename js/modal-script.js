1870a42cdd5e9f6835e8b6263bc6b7c8142305ea
Nikita Belevich <belevish_1998@mail.ru>
2020-08-22 11:15:47 +0300

    refactor: Убран лишний код, оставшийся оптимизирован
    Теперь есть основная функция закрытия окна, она закрывает вообще все возможные модальные окна, т.е элементы с определённым классом, тем самым вызов этой функции заменяет дублирование кода, это и было сделано.

--- js/modal-script.js
+++ js/modal-script.js
@@ -6,14 +6,13 @@ document.addEventListener('click', event => {
     if (!targetButtonShow) return;
     // Уверены что это была нажата кнопка вызова окна, запускаем функцию показа
     showModal.call(targetButtonShow); // Контекстом будет целевая кнопка
-
     // При каждом открытии окна, запускаем слушатель нажатия клавиш
     document.addEventListener('keydown', closeModalOnKey);
 });
 
 function showModal() {
     const typeModal = this.dataset.modal;
-    const modalWrap = document.querySelector(typeModal).parentElement;
+    const modalWrap = document.querySelector(typeModal);
     // Показываем наш оверлей и окно
     modalWrap.classList.remove('hide-modal');
     // Убираем прокрутку
@@ -22,55 +21,40 @@ function showModal() {
 
 //TODO Закрытие модального окна ---------------------------------------------------------------------
 const modalWindow = document.querySelector('.modal-window');
+const modalWrap = document.querySelector('.modal-wrap');
+const allModal = document.querySelectorAll('.modal-wrap');
+
 modalWindow.addEventListener('click', event => {
     const targetCloseButton = event.target.closest('.close-modal');
     if (!targetCloseButton) return;
-
-    closeModal.call(targetCloseButton);
+    closeModal();
+    document.removeEventListener('keydown', closeModalOnKey);
 });
-const modalWrap = document.querySelector('.modal-wrap');
 modalWrap.addEventListener('click', event => {
-    // Если был клик именно по оверлею, тогда скрываем окно
-    closeModalWrap.call(modalWrap);
+    // Если был клик именно по оверлею, а не по окну тогда скрываем всё
+    if (event.target === modalWrap) {
+        closeModal();
+        document.removeEventListener('keydown', closeModalOnKey);
+    }
 });
 
 
-const allModal = document.querySelectorAll('.modal-wrap');
+// TODO Закрытие всех окон и возврат прокрутки
+function closeModal() {
+    // Закрываем все возможные модальные окна
+    allModal.forEach(modal => {
+        modal.classList.add('hide-modal');
+    });
+    // Возвращаем прокрутку
+    document.body.style.overflowY = '';
+}
 // TODO Закрытие окна на Escape
 function closeModalOnKey(event) {
     if (event.code == 'Escape') {
         // Скрываем все возможные окна и возвращаем прокрутку
-        allModal.forEach(wrap => {
-            wrap.classList.add('hide-modal');
-        });
-        document.body.style.overflowY = '';
+        closeModal();
         // Если нажали Esc, удаляем этот обработчик с документа
         document.removeEventListener('keydown', closeModalOnKey);
     }
     // console.warn(event.code);
 }
-// TODO Закрытие окна на крестик
-function closeModal() {
-    const typeModal = this.dataset.modal;
-    const modalWrap = document.querySelector(typeModal).parentElement;
-    // Показываем наш оверлей и окно
-    modalWrap.classList.add('hide-modal');
-    // Возвращаем прокрутку
-    document.body.style.overflowY = '';
-    // При закрытии, удаляем обработчик закрытия окна по нажатию клавиши
-    document.removeEventListener('keydown', closeModalOnKey);
-}
-// TODO Закрытие окна по клику на оверлей
-function closeModalWrap() {
-    // Если целевой элемент у нас это именно оверлей, а не окно, тогда скрываем всё
-    if (event.target === modalWrap) {
-        event.target.classList.add('hide-modal');
-        document.body.style.overflowY = '';
-         // При закрытии, удаляем обработчик закрытия окна по нажатию клавиши
-        document.removeEventListener('keydown', closeModalOnKey);
-    }
-}
-
-
-
-
